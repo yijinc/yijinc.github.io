6cbacdb4e3e9a64503518150b6018f25862a3fc4
yijinc <yijinc@qq.com>
2017-12-12 17:40:31 +0800

    面向对象 music空间

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+
+var musicData = [
+    {
+        name : "赵雷 - 成都",
+        url : "http://sc1.111ttt.com/2016/1/12/04/205041718593.mp3"
+    },
+    {
+        name : "宋冬野 - 安和桥",
+        url : "http://up.mcyt.net/md5/17/NTAwMjY4Nw_Qq4329912.mp3"
+    },
+    {
+        name : "陈鸿宇 - 理想三旬",
+        url : "http://sc1.111ttt.com/2015/5/08/15/101151853006.mp3"
+    },
+    {
+        name: "谢春花 - 借我",
+        url : "http://up.mcyt.net/md5/53/NzA0MDkxMg_Qq4329912.mp3"
+    },
+    {
+        name : "灿烈 - All of me",
+        url : "http://sc1.111ttt.com/2015/1/11/16/104161559595.mp3"
+    },
+    {
+        name :"黄英华 - 只要为你活一天",
+        url : "http://sc1.111ttt.com/2015/1/05/02/98022140539.mp3"
+    },
+	{
+		name: "艾伦-Faded",
+		url: " http://sc1.111ttt.com/2015/1/12/31/105310629118.mp3"
+	},
+	{
+		name: "We Are Again",
+		url: "http://sc1.111ttt.com/2015/1/07/23/100232053131.mp3"
+	}
+];
+
+
 window.onload = function(){
 
 	$.get('https://yijinc.github.io/home/fakeData/skill.json').then(function (res) {
@@ -15,109 +52,9 @@ window.onload = function(){
 
     var head = document.getElementById("photo_wrap");
     head.style.left = (document.documentElement.clientWidth - head.offsetWidth)/2 +"px";
-  	var musicData = [
-  		{
-  			name : "赵雷 - 成都",
-  			url : "http://sc1.111ttt.com/2016/1/12/04/205041718593.mp3"
-  		},
-	  	{
-	  		name : "宋冬野 - 安和桥",
-	  		url : "http://up.mcyt.net/md5/17/NTAwMjY4Nw_Qq4329912.mp3"
-	  	},
-	  	{
-  			name : "陈鸿宇 - 理想三旬",
-  			url : "http://sc1.111ttt.com/2015/5/08/15/101151853006.mp3"
-  		},
-  		{
-  			name: "谢春花 - 借我",
-  			url : "http://up.mcyt.net/md5/53/NzA0MDkxMg_Qq4329912.mp3"
-  		},
-  		{
-  			name : "日向大介 - 24／7",
-  			url : "http://sc1.111ttt.com/2015/1/09/25/102251005125.mp3"
-  		},
-  		{
-  			name :"黄英华 - 只要为你活一天",
-  			url : "http://sc1.111ttt.com/2015/1/05/02/98022140539.mp3"
-  		}
-  	],
-  	iNow = 0;
+    $.drap(head);
 
-    var audio = document.createElement('audio');
-    var musicBox = document.getElementById('music');
-    var musicName = musicBox.getElementsByTagName("p")[0];
-    var musicManu = musicBox.getElementsByTagName("li");
-
-  	//初始化
-  	iNow = Math.floor(Math.random()*musicData.length);
-  	audio.src = musicData[iNow].url;
-  	musicName.innerHTML = musicData[iNow].name;
-
-  	audio.onloadeddata = function(){
-    	audio.play();
-  	}
-
-  	for(var i=1; i<musicManu.length-1; i++){
-      	musicManu[i].addEventListener("mouseover", function(){
-        	$.startMove(this.getElementsByTagName("strong")[0],{"opacity":100});
-        	$.startMove(musicBox.getElementsByTagName("p")[0],{"opacity":100});
-      	});
-      	musicManu[i].addEventListener("mouseout", function(){
-        	$.startMove(this.getElementsByTagName("strong")[0],{"opacity":0});
-        	$.startMove(musicBox.getElementsByTagName("p")[0],{"opacity":0});
-      	});
-  	}
-
-  	musicManu[0].onclick =  function(){
-    	var oWrap = musicBox.getElementsByTagName("div")[0];
-    	$.startMove(musicBox,{"left":-210},function(){
-    		oWrap.style.width=="30px"? oWrap.style.width="210px":oWrap.style.width="30px";
-    		$.startMove(musicBox,{"left":0});
-    	});
-    }
-
-  	musicManu[1].onclick = function(){
-    	iNow <= 0 ? iNow=musicData.length-1 : iNow--;
-    	audio.src = musicData[iNow].url;
-  		musicName.innerHTML = musicData[iNow].name;
-  	}
-
-  	musicManu[2].onclick = function(){
-    	var thatSpan = this.getElementsByTagName("span")[0];
-    	audio.paused ? 
-      		(function() {
-        		audio.play();
-        		thatSpan.className="music_c2";
-      		})() : 
-      		(function() {
-        		audio.pause();
-        		thatSpan.className="music_c1";
-      		})();
-  	}
-
-  	musicManu[3].onclick = function(){
-    	iNow >= musicData.length-1 ? iNow=0 : iNow++ ;
-    	audio.src = musicData[iNow].url;
-  		musicName.innerHTML = musicData[iNow].name;
-  	}
-
-  	musicManu[4].onclick = function(){
-    	this.getElementsByTagName("span")[0].className = (function() {
-        	return audio.muted ? "music_e1" : "music_e2";
-      	})();
-    	audio.muted = !audio.muted;
-  	}
-
-  	musicManu[5].onclick = function(event){
-    	var L = this.offsetLeft;
-    	var sound = this.getElementsByTagName("div")[0];
-    	var event = event || window.event;
-    	var W = event.clientX - L- musicBox.offsetLeft;
-    	sound.style.width = W +"px";
-    	audio.volume = W/50;
-  	}
-    
-    $.drap(document.getElementById("photo_wrap"));
+    new Music(musicData);
 
 }
 
--- js/music.js
+++ js/music.js
@@ -2,3 +2,133 @@
  * Created by yijinc on 2017/12/11
  */
 
+var Music = function (arr) {
+    this.init(arr);
+};
+
+Music.prototype = {
+
+    init: function (arr) {
+
+        this.iNow = 0;
+        this.list = arr;
+        this.createBox();
+
+        //初始化
+        this.iNow = Math.floor(Math.random()*this.list.length);
+        this.play(this.iNow);
+
+        this.audio.onloadeddata = ()=>this.audio.play();
+
+        for(let i=1; i<this.mMenu.length-1; i++){
+            this.mMenu[i].addEventListener("mouseover", () => {
+                $.startMove(this.mMenu[i].getElementsByTagName("strong")[0],{"opacity":100});
+                $.startMove(this.mInfo,{"opacity":100});
+            });
+            this.mMenu[i].addEventListener("mouseout", () => {
+                $.startMove(this.mMenu[i].getElementsByTagName("strong")[0],{"opacity":0});
+                $.startMove(this.mInfo,{"opacity":0});
+            });
+        }
+
+        this.mMenu[0].onclick = this.toggle.bind(this);
+        this.mMenu[1].onclick = this.pre.bind(this);
+        this.mMenu[2].onclick = this.pause.bind(this);
+        this.mMenu[3].onclick = this.next.bind(this);
+        this.mMenu[4].onclick = this.mute.bind(this);
+        this.mMenu[5].onclick = this.volumeChange.bind(this);
+
+    },
+
+    createBox: function () {
+
+        this.audio = document.createElement('audio');
+
+        this.mBox = document.createElement('div');
+        this.mBox.id = 'music';
+        this.mBox.innerHTML = `
+            <p style="opacity: 0;">拥抱 - 谭维维</p>
+            <div class="wrap" style="width:30px">
+                <ul>
+                    <li>
+                        <span class="music_a1"></span>
+                        <strong style="opacity: 1;"></strong>
+                    </li>
+                    <li>
+                        <span class="music_b1"></span>
+                        <strong style="opacity: 0;"></strong>
+                    </li>
+                    <li>
+                        <span class="music_c1"></span>
+                        <strong style="opacity: 0;"></strong>
+                    </li>
+                    <li>
+                        <span class="music_d1"></span>
+                        <strong style="opacity: 0;"></strong>
+                    </li>
+                    <li>
+                        <span class="music_e1"></span>
+                        <strong></strong>
+                    </li>
+                    <li class="sound">
+                        <div class="sound1"></div>
+                        <div class="sound2"></div>
+                    </li>
+                </ul>
+            </div>`;
+        this.mInfo = this.mBox.getElementsByTagName("p")[0];
+        this.oWrap = this.mBox.getElementsByTagName("div")[0];
+        this.mMenu = this.mBox.getElementsByTagName("li");
+        document.body.appendChild(this.mBox);
+    },
+
+    toggle: function () {
+        $.startMove(this.mBox, { "left" : -210 }, () => {
+            this.oWrap.style.width === "30px" ? this.oWrap.style.width = "210px" : this.oWrap.style.width = "30px";
+            $.startMove(this.mBox, {"left" : 0 } );
+        });
+    },
+
+    play: function (index) {
+        this.audio.src = this.list[index].url;
+        this.mInfo.innerHTML = this.list[index].name;
+    },
+
+    pre: function () {
+        this.iNow <= 0 ? this.iNow=this.list.length-1 : this.iNow--;
+        this.play(this.iNow);
+    },
+
+    next: function () {
+        this.iNow >= this.list.length-1 ? this.iNow=0 : this.iNow++ ;
+        this.play(this.iNow)
+    },
+
+    pause: function () {
+        const thatSpan = this.mMenu[2].getElementsByTagName("span")[0];
+        this.audio.paused ?
+            (()=>{
+                this.audio.play();
+                thatSpan.className = "music_c2";
+            })():
+            (()=>{
+                this.audio.pause();
+                thatSpan.className="music_c1";
+            })();
+    },
+
+    mute: function () {
+        this.mMenu[4].getElementsByTagName("span")[0].className = this.audio.muted ? "music_e1" : "music_e2";
+        this.audio.muted = !this.audio.muted;
+    },
+
+    volumeChange: function (event) {
+        let L = this.mMenu[5].offsetLeft;
+        const sound = this.mMenu[5].getElementsByTagName("div")[0];
+        event = event || window.event;
+        let W = event.clientX - L- this.mBox.offsetLeft;
+        sound.style.width = W +"px";
+        this.audio.volume = W/50;
+    }
+
+};
