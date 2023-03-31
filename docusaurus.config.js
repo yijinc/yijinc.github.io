5c5e8cb33dd59a9956cff300c6f3182342f49437
yijinc <yijinc@qq.com>
2023-03-31 14:29:49 +0800

    feat: use sass replace css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,11 +53,13 @@ const config = {
           editUrl: 'https://github.com/yijinc/yijinc.github.io',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/custom.scss'),
         },
       }),
     ],
   ],
+
+  plugins: ['docusaurus-plugin-sass'],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
